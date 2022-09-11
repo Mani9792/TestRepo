bcf77d12238aafb439d5e6248a2b388ee78b6994
Manikandan <manikandan.sl.9792@gmail.com>
2022-09-11 20:09:11 +0530

    Updated File

--- test.java
+++ test.java
@@ -11,8 +11,7 @@ public class Sample1 {
 		System.setProperty("webdriver.chrome.driver","D:\\MY PROJECT\\Source Files\\Drivers\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://www.selenium.dev");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.close();
+		driver.quit();
 
 	}
 
