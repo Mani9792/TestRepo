f2074b9a569ee94786b52bd6531630fc1bc072ae
Mani9792 <106470501+Mani9792@users.noreply.github.com>
2022-09-11 20:00:52 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,19 @@
+package sel_sample_browsers;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\MY PROJECT\\Source Files\\Drivers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.selenium.dev");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.close();
+
+	}
+
+}
